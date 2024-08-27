05eaeecc03af1d872069b73c2160439e344da37e
naman360 <naman.geek@gmail.com>
2024-08-27 23:18:48 +0530

    feat: added users api endpoint + refactored routers

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(morgan("dev"));
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`));
 
 const getAllTours = (req, res) => {
   res.status(200).json({
@@ -17,7 +18,7 @@ const getAllTours = (req, res) => {
 };
 
 const getTourById = (req, res) => {
-  const tourId = req.params.id * 1;
+  const userId = req.params.id * 1;
   const tour = tours.find((tour) => tour.id === tourId);
 
   if (!tour)
@@ -76,12 +77,92 @@ const createTour = (req, res) => {
   );
 };
 
-app.route("/api/v1/tours").get(getAllTours).post(createTour);
-app
-  .route("/api/v1/tours/:id")
-  .get(getTourById)
-  .patch(updateTour)
-  .delete(deleteTour);
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    totalTours: users.length,
+    data: { users },
+  });
+};
+
+const getUserById = (req, res) => {
+  const userId = req.params.id * 1;
+  const user = users.find((user) => user.id === userId);
+
+  if (!user)
+    return res.status(404).json({ status: "fail", message: "Invalid tour ID" });
+
+  res.status(200).json({
+    status: "success",
+    totalUser: users.length,
+    data: { user },
+  });
+};
+
+const updateUser = (req, res) => {
+  const userId = req.params.id * 1;
+  const user = users.find((user) => user.id === userId);
+
+  if (!user)
+    return res.status(404).json({ status: "fail", message: "Invalid user ID" });
+
+  res.status(200).json({
+    status: "success",
+    data: user,
+  });
+};
+
+const deleteUser = (req, res) => {
+  const userId = req.params.id * 1;
+  const user = users.find((user) => user.id === userId);
+
+  if (!user)
+    return res.status(404).json({ status: "fail", message: "Invalid user ID" });
+
+  res.status(200).json({
+    status: "success",
+    data: null,
+  });
+};
+
+const createUser = (req, res) => {
+  const newId = users[users.length - 1].id + 1;
+  const newUser = {
+    id: newId,
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    address: {
+      street: req.body.address.street,
+      city: req.body.address.city,
+      state: req.body.address.state,
+      zip: req.body.address.zip,
+    },
+    dateOfBirth: req.body.dateOfBirth,
+  };
+
+  users.push(newUser);
+
+  fs.writeFile("${__dirname}/data/users.json", JSON.stringify(users), (err) =>
+    res.status(201).json({
+      status: "success",
+      totalUsers: users.length,
+      data: { users: newUser },
+    })
+  );
+};
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+tourRouter.route("/").get(getAllTours).post(createTour);
+tourRouter.route("/:id").get(getTourById).patch(updateTour).delete(deleteTour);
+
+userRouter.route("/").get(getAllUsers).post(createUser);
+userRouter.route("/:id").get(getUserById).patch(updateUser).delete(deleteUser);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
