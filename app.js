a770ab93df3ed041d60720c8f787d4b7d059eb15
naman360 <naman.geek@gmail.com>
2024-08-28 20:11:50 +0530

    feat: environment variables

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const app = express();
 
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
-app.use(morgan("dev"));
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
-const PORT = process.env.PORT || 8000;
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
