3d1d5bbcd0d7fe85ff2a17696832f65f307f89fd
naman360 <naman.geek@gmail.com>
2024-08-29 18:14:48 +0530

    feat: db setup

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./.env" });
+const db = process.env.DB_URI?.replace("<PASSWORD>", process.env.DB_PASSWORD);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    console.log("Database Connected!");
+  });
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
