65c7fa56131847fe5d9723df19bf03c221ad67cf
naman360 <naman.geek@gmail.com>
2024-09-17 23:19:13 +0530

    feat: rate limiter middleware added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const AppError = require("./utils/appError");
@@ -7,6 +8,14 @@ const globalErrorHandler = require("./middlewares/error-handler");
 const app = express();
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
