1ea942447dde758ac990b351225570d994469422
naman360 <naman.geek@gmail.com>
2024-10-01 19:44:48 +0530

    feat: added middleware for security

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./middlewares/error-handler");
+const mongoSanitize = require("express-mongo-sanitize");
 const app = express();
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 const limiter = rateLimit({
-  max: 3,
+  max: 10,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
 
 app.use("/api", limiter);
 app.use(express.json());
-
+app.use(mongoSanitize());
+app.use(xss());
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
